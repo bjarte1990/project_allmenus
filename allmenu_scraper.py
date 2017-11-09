bc8d777f5f37181cf80a3e002c0a20b6b33975d8
Csaba Pigler <csaba.pigler@gmail.com>
2017-11-09 10:51:01 +0000

    checking for all filters
    modified:   allmenu_scraper.py

--- allmenu_scraper.py
+++ allmenu_scraper.py
@@ -11,6 +11,7 @@ DELIVERY_FILTER = '-/?filters=filter_delivery'
 ONLINE_FILTER = '-/?filters=filter_online'
 BOTH_FILTER = '-/?filters=filter_online,filter_delivery'
 
+FILTER_LIST = [ALL_RESTAURANT_FILTER, DELIVERY_FILTER, ONLINE_FILTER, BOTH_FILTER]
 
 def get_soup_from_link(link):
     http = urllib3.PoolManager()
@@ -32,24 +33,25 @@ def get_cities_in_state(allmenus_state_link):
 
     for city in cities:
         matching_object = re.match('<a href="(.*)">(.*)</a>', str(city))
-        city_links[matching_object.group(2)] = ALLMENUS_LINK + matching_object.group(1) + ALL_RESTAURANT_FILTER
+        city_links[matching_object.group(2)] = ALLMENUS_LINK + matching_object.group(1) #+ ALL_RESTAURANT_FILTER
 
     return city_links
 
 
 def get_restaurants_for_city(link):
-    soup = get_soup_from_link(link)
-
-    #restaurant_list_div = soup.find_all('li', {'class': 'restaurant-list-item clearfix'})
-    restaurant_list_div = soup.find_all('h4', {'class': 'name'})
     restaurant_links = OrderedDict()
-    #return len(restaurant_list)
-    #P.F. Chang's
-    for restaurant in restaurant_list_div:
-        matching_object = re.match('<.*id="(?P<id>.*)" href="(?P<link>.*)">.*</a>', str(restaurant))
-        restaurant_links[matching_object.group('id')] = ALLMENUS_LINK + matching_object.group('link')
-    print(restaurant_links)
+    for filter in FILTER_LIST:
+        soup = get_soup_from_link(link + filter)
 
+        #restaurant_list_div = soup.find_all('li', {'class': 'restaurant-list-item clearfix'})
+        restaurant_list_div = soup.find_all('h4', {'class': 'name'})
+        #return len(restaurant_list)
+        #P.F. Chang's
+        for restaurant in restaurant_list_div:
+            matching_object = re.match('<.*id="(?P<id>.*)" href="(?P<link>.*)">.*</a>', str(restaurant))
+            restaurant_links[matching_object.group('id')] = ALLMENUS_LINK + matching_object.group('link')
+
+    print(len(restaurant_links))
 
 city_links = get_cities_in_state(ALLMENUS_LINK + '/' + STATE)
 
